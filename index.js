8498427c1e88051daca92d734b135b885b683e43
Marcus <marcus.shimotsu@gmail.com>
2021-11-01 11:48:53 -0500

    commit tree d05aba65748fe2fa1ffe9db5b651130c0a0965f5

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1 = 31;
+let num2 = 2;
+let num3 = 13;
+let num4 = 9;
 
-// Write your code here
+let multiply = num1 * num2;
+let random = Math.floor(Math.random()*100)+1;
+let mod = num3 % num4;
+let max = Math.max(10,20);
